edafdb987693f2250dbc2e578c7f7018052cbddb
Intimaria <intimaria@gmail.com>
2021-10-07 21:22:20 -0300

    updates on store and autoload

--- lib/polycon.rb
+++ lib/polycon.rb
@@ -4,7 +4,7 @@ module Polycon
   autoload :VERSION,  'polycon/version'
   autoload :Commands, 'polycon/commands'
   autoload :Model,    'polycon/model'
-  autoload :Storage,  'polycon/storage'
-  autoload :Files,    'polycon/files'
+  #autoload :Storage,  'polycon/storage' => replaced with use of Dry::Files gem in Store
+  autoload :Store,    'polycon/store'
 
 end
--- lib/polycon/store.rb
+++ lib/polycon/store.rb
@@ -1,5 +1,8 @@
 module Polycon
   module Store 
+    #TODO -> define custom errors 
+    #     -> return true or false instead of exception (some methods) & validate at Model level
+    #     -> import into Models?
     
     PATH = Dir.home+'/.polycon/'
 
@@ -17,14 +20,30 @@ module Polycon
     def self.save(obj)
       begin
         if obj then
-          @files.directory?(PATH+obj.path) ? @files.mkdir(PATH+obj.path) : @files.write(obj)
+          @files.directory?(PATH+obj.path) ? write_dir(PATH+obj.path) : write(obj)
         else 
           raise Dry::Files::Error, "Nil value argument." 
         end 
       rescue 
-        Dry::Files::Error
+        raise Dry::Files::Error
       end
     end
+
+    def self.read(obj)
+      return nil if @files.directory?(PATH+obj.path)
+      begin
+        File.open(PATH+obj.path, 'r') do |f| 
+          obj.surname   = f.gets.chomp
+          obj.name      = f.gets.chomp 
+          obj.phone     = f.gets.chomp 
+          obj.notes     = f.gets.chomp
+        end
+      rescue NoMethodError 
+      ensure 
+        #validation done at Model level
+        obj
+      end
+    end 
 
     def self.rename(old_name:, new_name:)
       begin
@@ -57,54 +76,50 @@ module Polycon
         if directory then
           Dir.entries(PATH+directory)
         else 
-          raise Dry::Files::IOError, "Nil value argument." 
+          raise Dry::Files::Error, "Nil value argument." 
         end 
       rescue  
         raise Dry::Files::Error
       end
     end 
 
-  protected
+    protected
 
-  def copy_dir(old_name,new_name)
-    begin
-      FileUtils.mv PATH+old_name.path, PATH+new_name.path
-  rescue  
-    raise Dry::Files::Error
-  end
-  end 
-  def copy_file(old_name,new_name)
-    begin
-        @files.cp PATH+old_name.path, PATH+new_name.path
-    rescue  
-      raise Dry::Files::Error
-    end
-  end 
-  # For appointments only
-  def self.write(obj)
-    begin
-      @files.write(PATH+obj.path, obj.surname)
-      @files.append(PATH+obj.path, obj.name) 
-      @files.append(PATH+obj.path, obj.phone)
-      @files.append(PATH+obj.path, obj.notes) if obj.notes
-    rescue 
-      raise Dry::Files::IOError 
-    end
-  end 
-
-  def self.read(obj)
-    begin
-      File.open(PATH+obj.path, 'r') do |f| 
-        obj.surname   = f.gets.chomp
-        obj.name      = f.gets.chomp 
-        obj.phone     = f.gets.chomp 
-        obj.notes     = f.gets.chomp
+    def copy_dir(old_name,new_name)
+      begin
+        FileUtils.mv PATH+old_name.path, PATH+new_name.path
+      rescue  
+        raise Dry::Files::Error
       end
-      obj
-    rescue NoMethodError 
-    ensure 
-      obj
     end
+
+    def copy_file(old_name,new_name)
+      begin
+          @files.cp PATH+old_name.path, PATH+new_name.path
+      rescue  
+        raise Dry::Files::Error
+      end
+    end 
+
+    def self.write_dir(obj)
+      begin 
+        raise Dry::Files::Error if @files.directory?(PATH+obj.path)
+        @files.mkdir(PATH+obj.path)
+      rescue 
+         raise Dry::Files::Error
+      end 
+    end 
+
+    def self.write(obj)
+      begin
+        @files.write(PATH+obj.path, obj.surname)
+        @files.append(PATH+obj.path, obj.name) 
+        @files.append(PATH+obj.path, obj.phone)
+        @files.append(PATH+obj.path, obj.notes) if obj.notes
+      rescue 
+        raise Dry::Files::Error 
+      end
+    end 
+
   end 
-  
 end 
