8176fae8743fbab261804bfbca91f7f9ae676a5f
Intimaria <intimaria@gmail.com>
2021-10-27 18:29:27 -0300

    Add error for export

--- lib/polycon/exports/export.rb
+++ lib/polycon/exports/export.rb
@@ -6,5 +6,11 @@ module Polycon
     def self.export_week(appointments:, **options)
       puts "here I export all the appointments for a week"
     end 
+    #Export Errors
+    class ExportError 
+      def message
+        "there was a problem processing your export"
+      end; 
+    end 
   end 
 end 
