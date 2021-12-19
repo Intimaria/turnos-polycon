c91e48275766302db5967cfd8838cc8615f9128b
Intimaria <intimaria@gmail.com>
2021-12-18 21:36:01 -0300

    use counter_cache for total appointments for professional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
-
+  def configure_permitted_parameters
+  end
 end
